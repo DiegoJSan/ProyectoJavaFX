192cb1693472864fb1eafcbbc7417cbdb5db4384
Diego <Diego@DESKTOP-SSSJE9C>
2021-01-21 21:18:58 +0100

    Guardamos el proyecto

--- src/main/java/diegosanchez/alien/Alien.java
+++ src/main/java/diegosanchez/alien/Alien.java
@@ -12,6 +12,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -36,28 +38,63 @@ public class Alien extends Application {
         Polygon picoNave = new Polygon(new double[]{
             100.0, 0.0,
             80.0, 50.0,
-            120.0, 50.0 });
+            120.0, 50.0 
+        });
         picoNave.setFill(Color.WHITE);
         //Cuerpo nave
         Rectangle cuerpoNave = new Rectangle (80,50,40,100);
-        cuerpoNave.setFill(Color.WHITE);
+        cuerpoNave.setFill(Color.CRIMSON);
         //Ala izquierda nave
         Polygon alaIzqNave = new Polygon(new double[]{
             80.0, 70.0,
             20.0, 180.0,
-            80.0, 150.0 });
+            80.0, 150.0 
+        });
         alaIzqNave.setFill(Color.WHITE);
         //Ala derecha nave
         Polygon alaDrchNave = new Polygon(new double[]{
             120.0, 70.0,
             120.0, 150.0,
-            180.0, 180.0 });
+            180.0, 180.0 
+        });
         alaDrchNave.setFill(Color.WHITE);
+        //Fuego nave 1
+        Polygon fuegoNave1 = new Polygon(new double[]{
+            100.0, 180.0,
+            90.0, 150.0,
+            110.0, 150.0 
+        });
+        fuegoNave1.setFill(Color.YELLOW);
+        //Fuego nave 2
+        Polygon fuegoNave2 = new Polygon(new double[]{
+            100.0, 170.0,
+            95.0, 150.0,
+            105.0, 150.0 
+        });
+        fuegoNave2.setFill(Color.RED);
+        //Ventana nave
+        Ellipse ventanaNave = new Ellipse(); {
+        ventanaNave.setCenterX(100);
+        ventanaNave.setCenterY(50);
+        ventanaNave.setRadiusX(8);
+        ventanaNave.setRadiusY(18);
+        }
+        ventanaNave.setFill(Color.CORNFLOWERBLUE);
+        //Linea nave
+        Line lineaNave = new Line (70, 102, 70, 102);
+        lineaNave.setStroke(Color.WHITE);
+        lineaNave.setStrokeWidth(4);
+        //lineaNave.setFill(Color.WHITE);
+        
         //Agregar partes de la nave al contenedor
         root.getChildren().add(picoNave);
         root.getChildren().add(cuerpoNave);
         root.getChildren().add(alaIzqNave);
         root.getChildren().add(alaDrchNave);
+        root.getChildren().add(fuegoNave1);
+        root.getChildren().add(fuegoNave2);
+        root.getChildren().add(ventanaNave);
+        root.getChildren().add(lineaNave);
     }
 
     public static void main(String[] args) {
